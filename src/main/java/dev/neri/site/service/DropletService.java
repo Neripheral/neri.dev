bed0da26480103d7089a54dda5fc8d904b0b1469
Neripheral <neripheral@gmail.com>
2024-05-22 17:00:45 +0200

    mod: replace DropletService interface with implementation

--- src/main/java/dev/neri/site/service/DropletService.java
+++ src/main/java/dev/neri/site/service/DropletService.java
@@ -1,11 +1,26 @@
 package dev.neri.site.service;
 
+import dev.neri.site.dao.DropletRepository;
 import dev.neri.site.entity.Droplet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface DropletService {
-    List<Droplet> findAll();
+@Service
+public class DropletService {
+    private final DropletRepository repository;
 
-    Droplet saveDroplet(Droplet newDroplet);
+    @Autowired
+    public DropletService(DropletRepository dropletRepository) {
+        repository = dropletRepository;
+    }
+
+    public List<Droplet> findAll() {
+        return repository.findAll();
+    }
+
+    public Droplet saveDroplet(Droplet newDroplet){
+        return repository.save(newDroplet);
+    }
 }
